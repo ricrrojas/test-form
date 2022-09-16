b64053ab8edaa80444aefd057c7e3cd12ef33495
Ricardo Rojas <ricardoe@backbase.com>
2022-09-16 03:29:14 -0500

    lo k sea

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ProductsPage } from './page/Products'
 
 function App() {
-  console.log('hola'
-  console.log('k ase')
-  console.log('k paso')
+  console.log('hola');
+  console.log('k ase');
+  console.log('k paso');
+  console.log('k paso 2'); 
 
   const queryClient = new QueryClient()
 
